c8fcfb070b6e318b4e5530f3e234a8caf8138a49
MOSW626 <ays6533@naver.com>
2023-04-04 14:57:07 +0900

    numpad

--- src/pages/Numpad.js
+++ src/pages/Numpad.js
@@ -1,112 +1,55 @@
 import React, { useState } from "react";
+import Keypad from "./Keypad";
 
-function Keypad({ onDigitPress }) {
-  const keypadButtons = [
-    ["1", "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"],
-    ["C", "0", "E"],
-  ];
-
-  const handleDigitPress = (digit) => {
-    onDigitPress(digit);
-  };
-
-  return (
-    <div>
-      {keypadButtons.map((row, rowIndex) => (
-        <div key={`row-${rowIndex}`}>
-          {row.map((button, colIndex) => (
-            <button
-              key={`button-${rowIndex}-${colIndex}`}
-              onClick={() => handleDigitPress(button)}
-            >
-              {button}
-            </button>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function Numpad({ navigation }) {
+const Numpad = ({ correctPassword }) => {
   const [digit1, setDigit1] = useState("");
   const [digit2, setDigit2] = useState("");
   const [digit3, setDigit3] = useState("");
   const [digit4, setDigit4] = useState("");
 
-  const handleDigitChange = (i, value) => {
-    switch (i) {
-      case 0:
-        setDigit1(value);
-        break;
-      case 1:
-        setDigit2(value);
-        break;
-      case 2:
-        setDigit3(value);
-        break;
-      case 3:
-        setDigit4(value);
-        break;
-      default:
-        break;
-    }
-  };
-
   const handleDigitPress = (digit) => {
+    // If Clear button is clicked
     if (digit === "C") {
       setDigit1("");
       setDigit2("");
       setDigit3("");
       setDigit4("");
-    } else if (digit === "E") {
-      navigation();
+    }
+    // If Enter button is clicked
+    else if (digit === "E") {
+      const inputPassword = digit1 + digit2 + digit3 + digit4;
+      if (inputPassword === correctPassword) {
+        handleNavigation("./Finish");
+      } else {
+        setDigit1("");
+        setDigit2("");
+        setDigit3("");
+        setDigit4("");
+      }
     } else {
+      // If a digit button is clicked
       if (digit1 === "") {
         setDigit1(digit);
-        return;
-      }
-
-      if (digit2 === "") {
+      } else if (digit2 === "") {
         setDigit2(digit);
-        return;
-      }
-
-      if (digit3 === "") {
+      } else if (digit3 === "") {
         setDigit3(digit);
-        return;
-      }
-
-      if (digit4 === "") {
+      } else if (digit4 === "") {
         setDigit4(digit);
-        return;
       }
     }
   };
 
-  const handleNavigation = () => {
-    // 이동할 페이지에 대한 로직을 작성합니다.
-    // 예시로, '/Finish' 페이지로 이동하는 코드를 작성합니다.
-    navigation.navigate("/Finish");
-  }
-
+  const handleNavigation = (path) => {
+    // handle navigation logic here
+  };
 
   return (
     <div>
-      <input
-        type="password"
-        value={`${digit1}${digit2}${digit3}${digit4}`}
-        readOnly
-      />
-
+      <input type="password" value={`${digit1}${digit2}${digit3}${digit4}`} disabled />
       <Keypad onDigitPress={handleDigitPress} />
-
-      <button onClick={() => handleDigitPress("C")}>Clear</button>
-      <button onClick={() => handleDigitPress("E")}>Enter</button>
     </div>
   );
-}
+};
 
 export default Numpad;
