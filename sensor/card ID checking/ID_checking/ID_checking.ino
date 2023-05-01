565e230f082c7405580219bec3c1b69e7802c06f
crately <crate0330@gmail.com>
2023-05-01 19:39:32 +0900

    ID checking and ID nfc

---
+++ sensor/card ID checking/ID_checking/ID_checking.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <MFRC522.h>   
+ 
+#define RST_PIN   9
+#define SS_PIN    10
+
+MFRC522 rc522(SS_PIN, RST_PIN);
+
+int i=0;
+
+void setup(){
+  Serial.begin(9600);
+  SPI.begin();
+  rc522.PCD_Init();            
+}
+
+void loop(){
+
+  if ( !rc522.PICC_IsNewCardPresent() || !rc522.PICC_ReadCardSerial() ) { 
+    //카드 또는 ID 가 읽히지 않으면 return을 통해 다시 시작하게 됩니다.
+    delay(500);
+    return;
+  }
+  
+  Serial.print("Card UID:");
+  
+  for (byte i = 0; i < 4; i++) {
+    Serial.print(rc522.uid.uidByte[i]);
+    Serial.print(" ");
+  }
+  Serial.println(" ");
+
+  if(rc522.uid.uidByte[0]==185 && rc522.uid.uidByte[1]==57 && rc522.uid.uidByte[2]==149 
+    && rc522.uid.uidByte[3]==110) {  // 여기에 CARD UID 를 자신의 카드에 맞는 값으로 변경해주세요
+    
+    Serial.println("<< OK !!! >>  Registered card...");
+    
+    delay(500);
+  }
+  else{
+    Serial.println("<< WARNING !!! >>  This card is not registered");
+    delay(500);
+  }
+
+  delay(100);
+}
--- sensor/nfc/nfc.ino
+++ sensor/nfc/nfc.ino
@@ -1,8 +1,3 @@
-#include <MFRC522.h>
-#include <MFRC522Extended.h>
-#include <deprecated.h>
-#include <require_cpp11.h>
-
 #include <SPI.h>
 #include <MFRC522.h>   
  
@@ -21,24 +16,16 @@ void setup(){
 
 void loop(){
 
-  if ( !rc522.PICC_IsNewCardPresent() || !rc522.PICC_ReadCardSerial() ) { 
+ if ( !rc522.PICC_IsNewCardPresent() || !rc522.PICC_ReadCardSerial() ) { 
     //카드 또는 ID 가 읽히지 않으면 return을 통해 다시 시작하게 됩니다.
-    delay(500);
+    
     return;
   }
-  
-  Serial.print("Card UID:");
-  
-  for (byte i = 0; i < 4; i++) {
-    Serial.print(rc522.uid.uidByte[i]);
-    Serial.print(" ");
-  }
-  Serial.println(" ");
 
-  if(rc522.uid.uidByte[0]==185 && rc522.uid.uidByte[1]==57 && rc522.uid.uidByte[2]==149 
-    && rc522.uid.uidByte[3]==110) {  // 여기에 CARD UID 를 자신의 카드에 맞는 값으로 변경해주세요
+  if(rc522.uid.uidByte[0]==241 && rc522.uid.uidByte[1]==53 && rc522.uid.uidByte[2]==192
+    && rc522.uid.uidByte[3]==190) {  // 여기에 CARD UID 를 자신의 카드에 맞는 값으로 변경해주세요
     
-    Serial.println("<< OK !!! >>  Registered card...");
+    Serial.println("<< OK !!! >>  Registered card...(학번)(이름)");
     
     delay(500);
   }
